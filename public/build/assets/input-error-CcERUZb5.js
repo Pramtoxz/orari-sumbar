dceaf0543365424656ed81585bcfacd7f0663cf9
Pramtoxz <pramuditometra@gmail.com>
2025-06-05 23:14:13 +0700

    oke

---
+++ public/build/assets/input-error-CcERUZb5.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Bht6Vi1J.js";import{c as o}from"./button-Oe6hvK4D.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
